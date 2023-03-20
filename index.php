3418fe5fd3f27cfa0087608b0f30988fa2cfad9f
Kya Timmins <ktimmins@nic.edu>
2023-03-20 14:20:45 -0700

    Basic Layout

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rubber Duck</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="rubber-duck"></div>
+</body>
+</html>
